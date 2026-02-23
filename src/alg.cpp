f425fbc3124d5ec16cf7894684f3dce6daa1b98a
zephyr2007 <amirkarimov.9@mail.ru>
2026-02-23 18:53:31 +0300

    ф

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,33 +1,97 @@
 // Copyright 2022 NNTU-CS
+#include <iostream>
+using namespace std;
+
 #include <cstdint>
 #include "alg.h"
 
 
 bool checkPrime(uint64_t value) {
-  for (int i = 2; i <= (value / 2) + 1; i+=1) {
-    if (value % i == 0) {
-      return false
+  if (value < 2) {
+    return false;
+  }
+  else if (value == 2) {
+    cout << value << " prime" << endl;
+  }
+  else {
+    for (int i = 2; i <= (value / 2) + 1; i += 1) {
+      if (value % i == 0) {
+        cout << value << " non-prime" << endl;
+        return false;
+      }
     }
+
+    cout << value << " prime" << endl;
+
   }
   return true;
 }
 
+
 uint64_t nPrime(uint64_t n) {
-  // вставьте код функции
-  return 2;
+  if (n == 0) return 0;
+
+  int count = 0;
+  uint64_t i = 1;
+
+  while (count < n) {
+    i++;
+    if (checkPrime(i)) {
+      count++;
+    }
+  }
+
+  return i;
 }
 
+
 uint64_t nextPrime(uint64_t value) {
-  // вставьте код функции
+  if (value <= 1) {
+    cout << "wrong input" << endl;
+  }
+  uint64_t a = value + 1;
+  while (1) {
+    if (checkPrime(a)) {
+      return a;
+    }
+    else {
+      a++;
+    }
+  }
+
   return 2;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  // вставьте код функции
-  return 2;
+  if (hbound <= 2) {
+    cout << "wrong input" << endl;
+    return -1;
+  }
+  uint64_t sum = 0;
+
+
+  for (int i = 2; sum < hbound; i++) {
+    if (checkPrime(i)) {
+      if (sum + i > hbound) {
+        cout << "sum = " << sum << endl;
+        return 0;
+      }
+      sum += i;
+    }
+  }
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-  // вставьте код функции
+  if ((lbound <= 1) || (lbound >= hbound - 2)) {
+    cout << "wrong input" << endl;
+    return 0;
+  }
+  uint64_t a = 0;
+  for (int i = lbound; i <= (hbound - 3); i++) {
+    if (checkPrime(i) && checkPrime(i + 2)) {
+      a += 1;
+    }
+  }
+  cout << "number of twins: " << a << endl;
   return 1;
 }
